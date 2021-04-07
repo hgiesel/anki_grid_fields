cdf431329e190410ab5975394351c1e8832dc90b
Henrik Giesel <hengiesel@gmail.com>
2021-04-07 17:51:58 +0200

    Add Settable column number + Zoomable fields with Shift+F10

--- src/editor.py
+++ src/editor.py
@@ -1,5 +1,7 @@
 from aqt.gui_hooks import (
     editor_will_load_note,
+    editor_did_init_buttons,
+    editor_did_init,
 )
 
 from .utils import grid_areas
@@ -12,5 +14,20 @@ def show_grid(js, note, editor):
     return f"{js}; gridFields.setCustomAreas(`{value}`); "
 
 
+def add_column_number_input(righttopbtns, editor):
+    righttopbtns.append(
+        """
+<label for="colCount">Cols:</label>
+<input type="number" value="0" min="0" max="9" name="colCount" id="colCount" />
+    """
+    )
+
+
+def init_column_number_input(editor):
+    editor.web.eval("gridFields.setupColCount(); ")
+
+
 def init_editor():
     editor_will_load_note.append(show_grid)
+    editor_did_init_buttons.append(add_column_number_input)
+    editor_did_init.append(init_column_number_input)
--- web/grid.js
+++ web/grid.js
@@ -1,88 +1,151 @@
-var gridFields = new class {
-    constructor() {
-        this.style = document.createElement("style");
+var gridFields = new (class {
+  constructor() {
+    this.style = document.createElement("style");
+    this.zoomedField = null;
+    document.head.appendChild(this.style);
 
-        document.head.appendChild(this.style);
+    document.addEventListener("keydown", (event) => {
+      if (event.code === "F10") {
+        if (event.shiftKey) {
+          this.toggleFieldZoom();
+        } else {
+          this.toggleGridFieldsMode();
+        }
+      }
+    });
+  }
 
-        document.addEventListener("keydown", (event) => {
-            if (event.code === "F10") {
-                if (this.defaultGrids) {
-                    bridgeCommand("getGridAreas", this.setCustomAreas.bind(this));
-                }
-                else {
-                    this.setDefaultColumnGrids();
-                }
-            }
-        })
+  toggleFieldZoom() {
+    const field = getCurrentField();
+
+    if (typeof this.zoomedField === "number") {
+      getEditorField(this.zoomedField).style.gridColumn = "";
+
+      if (field && this.zoomedField === field.ord) {
+        // unzoom
+        this.zoomedField = null;
+        return;
+      }
     }
 
-    setDefaultColumnGrids() {
-        const fields = document.getElementById("fields");
-
-        this.defaultGrids = true;
-        this.style.textContent = `
-@media (max-width: 350px) {
-  #fields {
-    grid-template-columns: 100%;
+    if (field) {
+      this.zoomedField = field.ord;
+      getEditorField(this.zoomedField).style.gridColumn = "1 / -1";
+    }
   }
-}
-`
 
-        if (fields.childElementCount >= 2) {
-            this.style.textContent += `
-@media (min-width: 350px) {
-  #fields {
-    grid-template-columns: 49.5% 49.5%;
+  toggleGridFieldsMode() {
+    if (this.defaultGrids) {
+      bridgeCommand("getGridAreas", this.setCustomAreas.bind(this));
+    } else {
+      const colCount = document.getElementById("colCount");
+      this.setColumnGrids(colCount.value);
+    }
   }
-}
-`
-        }
 
-        if (fields.childElementCount >= 3) {
-            this.style.textContent += `
-@media (min-width: 700px) {
-  #fields {
-    grid-template-columns: 33% 33% 33%;
+  setupColCount() {
+    const colCount = document.getElementById("colCount");
+    colCount.addEventListener("change", this.setColumnGridsEvent.bind(this));
   }
+
+  getFieldsTemplateColumnsCss(n) {
+    const baseValue = 99 / n;
+    const templateColumnsValue = Array.apply(0, Array(n))
+      .map((_) => `${baseValue}%`)
+      .join(" ");
+
+    return `
+#fields {
+    grid-template-columns: ${templateColumnsValue};
 }
-`
-        }
+`;
+  }
+
+  setDefaultColumnGrids() {
+    const fields = document.getElementById("fields");
+
+    this.defaultGrids = true;
+    this.style.textContent = `
+@media (max-width: 350px) { ${this.getFieldsTemplateColumnsCss(1)} }
+`;
+
+    if (fields.childElementCount >= 2) {
+      this.style.textContent += `
+@media (min-width: 350px) { ${this.getFieldsTemplateColumnsCss(2)} }
+`;
     }
 
-    numberToGridArea(n) {
-        return `
+    if (fields.childElementCount >= 3) {
+      this.style.textContent += `
+@media (min-width: 700px) { ${this.getFieldsTemplateColumnsCss(3)} }
+`;
+    }
+
+    if (fields.childElementCount >= 4) {
+      this.style.textContent += `
+@media (min-width: 1050px) { ${this.getFieldsTemplateColumnsCss(3)} }
+`;
+    }
+  }
+
+  setColumnGrids(value) {
+    this.defaultGrids = true;
+
+    switch (value) {
+      case 0:
+        this.setDefaultColumnGrids();
+        break;
+      default:
+        this.style.textContent = this.getFieldsTemplateColumnsCss(value);
+        break;
+    }
+  }
+
+  setColumnGridsEvent(event) {
+    const value = Number(event.currentTarget.value);
+    console.log(value);
+    this.setColumnGrids(value);
+    console.log(this.style.textContent);
+  }
+
+  numberToGridArea(n) {
+    return `
 #fields > :nth-child(${n}) {
     grid-area: f${n};
 }
-`
-    }
+`;
+  }
 
-    areasToCss(areas) {
-        const formatted = areas.split("\n").map(row => `"${row}"`).join("\n")
+  areasToCss(areas) {
+    const formatted = areas
+      .split("\n")
+      .map((row) => `"${row}"`)
+      .join("\n");
 
-        return `
+    return `
 #fields {
 grid-template-areas:
 ${formatted};
 }
-`
+`;
+  }
+
+  setCustomAreas(areas) {
+    if (!areas) {
+      const colCount = document.getElementById("colCount");
+      this.setColumnGrids(colCount.value);
+      return;
     }
 
-    setCustomAreas(areas) {
-        if (!areas) {
-            this.setDefaultColumnGrids();
-            return;
-        }
+    let css = "";
 
-        let css = "";
+    forEditorField([], (field) => {
+      css += this.numberToGridArea(field.editingArea.ord + 1);
+    });
 
-        forEditorField([], (field) => {
-            css += this.numberToGridArea(field.editingArea.ord + 1);
-        });
+    css += this.areasToCss(areas);
 
-        css += this.areasToCss(areas);
-
-        this.defaultGrids = false;
-        this.style.textContent = css;
-    }
-}
+    this.defaultGrids = false;
+    this.style.textContent = css;
+  }
+})();
